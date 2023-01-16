11a77cf9bfd2822a3ef4fa20e2aa9121eb893763
Anna Mityaeva <aiisa@inbox.ru>
2023-01-17 01:34:38 +0300

    task 64(2)

---
+++ task64_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.Write("Введите число M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"N={N}, M={M}");
+if (M>N)
+for (int i = M; i >= N; i--)
+    Console.Write($" {i}");
+else
+    for (int i = N; i >= M; i--)
+Console.Write($" {i}");
